2aa6635a26780208824db8c64060ceb39a7919b6
Hithesh Bheesetti <hitheshbheesetti@gmail.com>
2026-07-17 13:10:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.59%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        long long num=n;
+        if(n<0){
+            num= -1*num;
+        }
+
+        while(num>0){
+            if(num%2==1){
+                ans=ans*x;
+                num=num-1;
+
+            }
+            
+            else{
+                x=x*x;
+                num=num/2;
+            }
+
+        }
+
+        
+        
+        if(n<0){
+            ans=(double)(1/ans);
+        }
+
+
+
+        
+        return ans;
+    }
+};
