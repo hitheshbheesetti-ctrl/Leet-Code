4706470b8f8898125a3c71836177a513907c788f
Hithesh Bheesetti <hitheshbheesetti@gmail.com>
2026-09-02 11:30:51 +0530

    Time: 0 ms (100%), Space: 30.2 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -2,40 +2,41 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
 
-        int num1=0;
-        int num2=0;
+        // int num1=0;
+        // int num2=0;
 
-        for(int i=0;i<nums1.size();i++){
-            if(nums1[i]%2==0){
-                num1++;
-            }
-            else{
-                num2++;
-            }
-        }
+        // for(int i=0;i<nums1.size();i++){
+        //     if(nums1[i]%2==0){
+        //         num1++;
+        //     }
+        //     else{
+        //         num2++;
+        //     }
+        // }
 
-        for(int i=0;i<nums1.size();i++){
+        // for(int i=0;i<nums1.size();i++){
             
-            for(int j=0;j<nums1.size();j++){
-                int numb=abs(nums1[i]-nums1[j]);
-                if(numb%2==0 && j!=i){
-                    num1++;
-                }
-                else if((nums1[i]-nums1[j])%2!=0 && j!=i){
-                    num2++;
-                }
+        //     for(int j=0;j<nums1.size();j++){
+        //         int numb=abs(nums1[i]-nums1[j]);
+        //         if(numb%2==0 && j!=i){
+        //             num1++;
+        //         }
+        //         else if((nums1[i]-nums1[j])%2!=0 && j!=i){
+        //             num2++;
+        //         }
 
-            }
-        }
+        //     }
+        // }
 
 
-        if(num1>=nums1.size() || num2>=nums1.size()){
-            return true;
-        }
-        else{
-            return false;
-        }
+        // if(num1>=nums1.size() || num2>=nums1.size()){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
 
+        return true;
         
         
     }
